c394c7d3db2a07ce10d2efcc4e5ad2cb7c34b504
Hershit-shukla <109151752+Hershit-shukla@users.noreply.github.com>
2023-02-02 22:41:55 +0530

    Update 2: Print a number Entered by the user.c

--- 2: Print a number Entered by the user.c
+++ 2: Print a number Entered by the user.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+
 int main() {
     int number;
+    
     printf("Enter Number: ");
     scanf("%d",&number);
+    
     printf("You have Entered %d",number);
+    
     return 0;
 }
