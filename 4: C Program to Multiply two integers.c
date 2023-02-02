2f8a09bf9ed78ff9c687d57806430ecc70bfa2ab
Hershit-shukla <109151752+Hershit-shukla@users.noreply.github.com>
2023-02-02 22:43:03 +0530

    Update 4: C Program to Multiply two integers.c

--- 4: C Program to Multiply two integers.c
+++ 4: C Program to Multiply two integers.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+
 int main() {
     float number1, number2, Mul;
+    
     printf("Number 1 : ");
     scanf("%f",&number1);
+    
     printf("Number 2 : ");
     scanf("%f",&number2);
+    
     Mul = number1 * number2;
+    
     printf("Multiplication of %f and %f is %f",number1,number2,Mul);
+    
     return 0;
 }
