d2ac2139721c33178edd33313c655ceb7eb211a3
Hershit-shukla <109151752+Hershit-shukla@users.noreply.github.com>
2023-02-02 22:09:21 +0530

    Create 5: C program to find ASCII value of a given Character.c

---
+++ 5: C program to find ASCII value of a given Character.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    char chr;
+    printf("Enter the Character : ");
+    scanf("%c",&chr);
+    printf("\nThe ASCII Value of %c is %d", chr, chr);
+    return 0;
+}
